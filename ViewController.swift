852146eeff596d7cd06672bf73ab4fa9c9bfdd6f
Alireza <ali.ajourlooo@gmail.com>
2019-01-31 16:32:26 +0330

    starter project

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  slack_Clone
+//
+//  Created by shahin miraftabi on 1/31/19.
+//  Copyright © 2019 com.example. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
