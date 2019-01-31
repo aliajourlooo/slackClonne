ecd078cb0cb6db2589b2b0bd26d5d7889aeaea09
Alireza <ali.ajourlooo@gmail.com>
2019-01-31 18:28:50 +0330

    adding SWRevealController

---
+++ slack_Clone-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  slack_Clone-Bridging-Header.h
+//  slack_Clone
+//
+//  Created by shahin miraftabi on 1/31/19.
+//  Copyright © 2019 com.example. All rights reserved.
+//
+
+#import "SWRevealViewController.h"
