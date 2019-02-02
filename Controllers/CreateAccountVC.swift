a54aaf6988df00b4c7a1b1d77701a511fe51e36d
Alireza <ali.ajourlooo@gmail.com>
2019-02-02 18:29:32 +0330

    add createAccountVC and xib

---
+++ Controllers/CreateAccountVC.swift
@@ -0,0 +1,40 @@
+//
+//  CreateAccountVC.swift
+//  slack_Clone
+//
+//  Created by shahin miraftabi on 2/2/19.
+//  Copyright © 2019 com.example. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountVC: UIViewController {
+
+    //MARK : OUTLETS
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    @IBOutlet weak var usernameTxtField: UITextField!
+    @IBOutlet weak var emaiilTxtField: UITextField!
+    @IBOutlet weak var passwordTxtField: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    //MARK: Functions
+    @IBAction func closeBtnPressed(_ sender: Any) {
+        // we will use unwind
+    }
+    @IBAction func profileImageBtnPressed(_ sender: Any) {
+    }
+    @IBAction func generateColorBtnPressed(_ sender: Any) {
+    }
+    
+    // MARK: - Navigation
+
+}
---
+++ Controllers/LoginVC.swift
@@ -0,0 +1,32 @@
+//
+//  LoginVC.swift
+//  slack_Clone
+//
+//  Created by shahin miraftabi on 2/2/19.
+//  Copyright © 2019 com.example. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    //MARK: OUTLETS
+    
+    @IBOutlet weak var userNameTxtField: UITextField!
+    @IBOutlet weak var passwordTxtField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    //MARK: functions
+    @IBAction func closeBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    @IBAction func loginBtnPressed(_ sender: Any) {
+    }
+    
+
+}
