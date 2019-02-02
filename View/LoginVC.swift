5c38fb85fbe135f2e26693b91a9e202bbd06ef57
Alireza <ali.ajourlooo@gmail.com>
2019-02-02 16:08:15 +0330

    add loginVC file and connect

---
+++ View/LoginVC.swift
@@ -0,0 +1,31 @@
+//
+//  LoginVC.swift
+//  slack_Clone
+//
+//  Created by shahin miraftabi on 2/2/19.
+//  Copyright © 2019 com.example. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    
+    //MARK: OUTLETS
+    
+    @IBOutlet weak var userNameTxtField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    //MARK: VARIABLES
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func loginBtnPressed(_ sender: Any) {
+    }
+    
+
+}
